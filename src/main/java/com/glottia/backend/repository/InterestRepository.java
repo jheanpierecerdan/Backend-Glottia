6db77f357efeb7a726d034f855e5906486b674c1
Jose <>
2026-05-10 08:32:50 -0500

    feat: add Interest entity, repository, service and controller
    Co-Authored-By: Jose <Jose@DESKTOP-IATSGDI>

--- src/main/java/com/glottia/backend/repository/InterestRepository.java
+++ src/main/java/com/glottia/backend/repository/InterestRepository.java
@@ -1,4 +1,9 @@
 package com.glottia.backend.repository;
 
-public class InterestRepository {
+import com.glottia.backend.entity.Interest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InterestRepository extends JpaRepository<Interest, Integer> {
 }
