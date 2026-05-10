f47884c65137526f671f4fbf06c2f864fced9f1c
Ariana <>
2026-05-10 09:09:31 -0500

    feat: implementacion de entidades, repositorios, servicios y controladores (User, Role, Event, Reservation)
    Co-Authored-By: Ariana <Ariana@glottia.com>

--- src/main/java/com/glottia/backend/repository/RoleRepository.java
+++ src/main/java/com/glottia/backend/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package com.glottia.backend.repository;
 
-public class RoleRepository {
+import com.glottia.backend.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByNombre(String nombre);
 }
